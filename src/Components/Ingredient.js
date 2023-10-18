7738dca47b13faee1f68713bd46b0f9292d30ec8
dylantoporek <87386624+dylantoporek@users.noreply.github.com>
2023-10-18 11:01:06 -0400

    store v2 almost done

--- src/Components/Ingredient.js
+++ src/Components/Ingredient.js
@@ -1,41 +1,72 @@
 import React, {useState} from "react";
+import {Stack, Flex, Text, Button, Image, useMediaQuery, Input } from '@chakra-ui/react'
+import {motion} from 'framer-motion'
+import rupee from '../Images/rupee.png'
 import backgroundHover from "../Images/backgroundHover.png"
 import backgroundNoHover from "../Images/backgroundNoHover.png"
 
 function Ingredient({ing, setTogDetails, setSpecificIng}){
     const [isShown, setIsShown] = useState(false)
-
-    function putInCart(){ 
+    const [isMobile] = useMediaQuery("(max-width: 768px)", {
+        ssr: true,
+        fallback: false,
+    })
+    function openDetails(){ 
         setSpecificIng(ing)
         setTogDetails(true)
     }
 
-    let ingDisplay
-    if (ing.name !==null){
-        ingDisplay = <div 
-                onMouseEnter={()=> setIsShown(true)}
-                onMouseLeave={()=> setIsShown(false)}
-                onClick={putInCart} className='ingredient'>
-            <p className="ing-name">{ing.name}</p>
-            <img className="ing-img" src={ing.image} />
-            <img className='ing-background-noH' src={backgroundNoHover}/>
-        </div>
-    }
+    // let ingDisplay
+    // if (ing.name !==null){
+    //     ingDisplay = <div 
+    //             onMouseEnter={()=> setIsShown(true)}
+    //             onMouseLeave={()=> setIsShown(false)}
+    //             onClick={putInCart} className='ingredient'>
+    //         <p className="ing-name">{ing.name}</p>
+    //         <img className="ing-img" src={ing.image} />
+    //         <img className='ing-background-noH' src={backgroundNoHover}/>
+    //     </div>
+    // }
 
-    if (isShown){
-        ingDisplay = <div 
-            onMouseEnter={()=> setIsShown(true)}
-            onMouseLeave={()=> setIsShown(false)}
-            onClick={putInCart} className='ingredient'>
-            <p className="ing-name">{ing.name}</p>
-            <img className="ing-img" src={ing.image} />
-            <img className='ing-background-H' src={backgroundHover}/>
-        </div>
-    }
+    // if (isShown){
+    //     ingDisplay = <div 
+    //         onMouseEnter={()=> setIsShown(true)}
+    //         onMouseLeave={()=> setIsShown(false)}
+    //         onClick={putInCart} className='ingredient'>
+    //         <p className="ing-name">{ing.name}</p>
+    //         <img className="ing-img" src={ing.image} />
+    //         <img className='ing-background-H' src={backgroundHover}/>
+    //     </div>
+    // }
 
-    return (
-        <div>{ingDisplay}</div>
-    )
+    // return (
+    //     <div>{ingDisplay}</div>
+    // )
+    return(
+        <motion.div
+         whileHover={{scale: 1.1}}>
+            <Flex 
+             cursor={'pointer'}
+             flexDir={'column'} 
+             alignItems={'center'} 
+             backgroundColor={'rgba(255, 255, 255, .8)'}
+             boxShadow={'0px 1px 2px 2px rgba(54,54,54,.5)'}
+             w={isMobile ? '150px':'200px'}
+             h={'200px'}
+             mb={5}
+             onClick={() => openDetails()}>
+                
+                <Image mt={5} w={'60px'} h={'60px'} src={ing.image}/>
+                <Text textAlign={'center'} mt={5} fontSize={isMobile ? 12 : 14}>
+                    {ing.name}
+                </Text>
+                <Flex gap={1}>
+                    <Image w={'10px'} src={rupee}/>
+                    <Text>{ing.price}</Text>
+                </Flex>
+            </Flex>
+        </motion.div>
+    ) 
     
 }
 
--- src/Pages/Store.js
+++ src/Pages/Store.js
@@ -1,4 +1,6 @@
 import React, {useState, useEffect} from "react";
+import {Stack, Flex, Text, Button, Image, useMediaQuery, Link } from '@chakra-ui/react'
+import {motion} from 'framer-motion'
 import Ingredient from "../Components/Ingredient";
 import StoreNav from "../Components/StoreNav";
 import Details from "../Components/Details";
@@ -9,7 +11,10 @@ function Store({ingredientList, addItemToCart, changePage}){
     const [nameFilter, setNameFilter] = useState("")
     const [togDetails, setTogDetails] = useState(false)
     const [specificIng, setSpecificIng] = useState(null)
-
+    const [isMobile] = useMediaQuery("(max-width: 768px)", {
+        ssr: true,
+        fallback: false,
+    })
     useEffect(()=>{
         changePage(window.location.href)
     }, [])
@@ -33,45 +38,96 @@ function Store({ingredientList, addItemToCart, changePage}){
         }
     })
 
-    const ingredientDisplay = filteredByName.map((ing)=>{
-        return <Ingredient key={ing.id} ing={ing} setTogDetails={setTogDetails} setSpecificIng={setSpecificIng}/>
-    })
+    // const ingredientDisplay = filteredByName.map((ing)=>{
+    //     return <Ingredient key={ing.id} ing={ing} setTogDetails={setTogDetails} setSpecificIng={setSpecificIng}/>
+    // })
 
-    const detailsDisplay = specificIng ? <Details addItemToCart={addItemToCart} setTogDetails={setTogDetails} ing={specificIng}/> : null
+    // const detailsDisplay = specificIng ? <Details addItemToCart={addItemToCart} setTogDetails={setTogDetails} ing={specificIng}/> : null
     
     
-    if(!togDetails){
-        return <div id='page-background'>
-<div className="store-comp-cont-1">
-        <StoreNav 
-            name={nameFilter} 
-            category={categoryFilter} 
-            setCategoryFilter={setCategoryFilter}
-            setNameFilter={setNameFilter}
-        />
-        {ingredientDisplay}
-    </div>
-    <img id='login-signup-background' src={greyBackground} />
-        </div>
+//     if(!togDetails){
+//         return <div id='page-background'>
+// <div className="store-comp-cont-1">
+//         <StoreNav 
+//             name={nameFilter} 
+//             category={categoryFilter} 
+//             setCategoryFilter={setCategoryFilter}
+//             setNameFilter={setNameFilter}
+//         />
+//         {ingredientDisplay}
+//     </div>
+//     <img id='login-signup-background' src={greyBackground} />
+//         </div>
         
-    } else{
-        return <div id='page-background'>
+//     } else{
+//         return <div id='page-background'>
 
-<div className="store-comp-cont-1">
-        <StoreNav 
-            name={nameFilter} 
-            category={categoryFilter} 
-            setCategoryFilter={setCategoryFilter}
-            setNameFilter={setNameFilter}
-        />
-        {detailsDisplay}
-        {ingredientDisplay}
+// <div className="store-comp-cont-1">
+//         <StoreNav 
+//             name={nameFilter} 
+//             category={categoryFilter} 
+//             setCategoryFilter={setCategoryFilter}
+//             setNameFilter={setNameFilter}
+//         />
+//         {detailsDisplay}
+//         {ingredientDisplay}
         
-    </div>
-    <img id='login-signup-background' src={greyBackground} />
-        </div>
+//     </div>
+//     <img id='login-signup-background' src={greyBackground} />
+//         </div>
         
-    }
+//     }
+return (
+    <Stack
+     flexDir={'row'}
+     maxW={'100vw'} 
+     mt={isMobile ? '55px':'65px'} 
+     p={isMobile ? 0:10}
+     backgroundColor={'#20606F'}
+     minH={'90.5vh'}
+     justifyContent={isMobile ? 'center':'flex-end'}>
+        <motion.div
+        style={{
+            display: 'flex',
+            flexDirection: 'row'
+         }}
+         initial={{opacity: 0}}
+         animate={{opacity: 1}}>
+            <Flex>
+                <StoreNav 
+                    nameFilter={nameFilter} 
+                    category={categoryFilter}
+                    setNameFilter={setNameFilter}
+                    setCategoryFilter={setCategoryFilter}/>
+            </Flex>
+            <Flex
+                overflow={togDetails ? 'hidden' : 'unset'}
+                h={togDetails ? '100vh' : '100%'}
+                transition={'height .2s ease-in-out'}
+                w={isMobile ? '100vw':'70vw'}
+                p={0}
+                position={'relative'}
+                flexWrap={'wrap'} 
+                top={isMobile ? '180px': 0} 
+                paddingY={10}
+                gap={5}
+                zIndex={0}
+                left={isMobile ? 0 : 0}
+                ml={isMobile ? 0:'3vw'}
+                mr={isMobile ? 0:'1vw'}
+                justifyContent={isMobile ? 'center' : 'center'}
+                backgroundColor={'#20606F'}>
+                    {filteredByName.map((ingredient) => 
+                     ingredient.name ? 
+                        <Ingredient 
+                        key={ingredient.id}
+                        ing={ingredient} 
+                        setTogDetails={setTogDetails} 
+                        setSpecificIng={setSpecificIng}/> : null)}
+            </Flex> 
+        </motion.div>
+    </Stack>
+)
     
 }
 
